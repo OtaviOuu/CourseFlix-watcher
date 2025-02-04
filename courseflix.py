aa07cdd7fb795f94a7436c239da720668d748b81
OtaviOuu <conta.otaviou@gmail.com>
2025-02-04 19:45:03 -0300

    Refactor CourseFlixManager to use JSON for course storage and update methods for saving new courses

--- courseflix.py
+++ courseflix.py
@@ -1,5 +1,6 @@
 import requests
 from parsel.selector import Selector
+import json
 
 
 class CourseFlixManager:
@@ -7,22 +8,22 @@ class CourseFlixManager:
         self.base_url = "https://courseflix.net/"
 
     def _load_stored_courses(self) -> list:
-        with open("courses.txt", "r") as file:
-            return [line.rstrip() for line in file]
+        with open("courses.json", "r") as file:
+            return json.load(file)["courses"]
 
     def _get_selector(self, html_text) -> Selector:
         return Selector(html_text)
 
-    def _write_courses(self, courses: list):
-        with open("courses.txt", "a") as file:
-            for course in courses:
-                file.write(course + "\n")
-
-    def _write_to_counter(self, new_courses_quantity: int):
-        with open("new.txt", "r") as file:
-            new_courses_stack = int(file.read().strip())
-        with open("new.txt", "w") as file:
-            file.write(str(new_courses_stack + new_courses_quantity))
+    def _save_new_courses(self, new_courses: list[str]) -> None:
+        try:
+            with open("courses.json", "r+") as file:
+                past_courses = json.load(file)
+                past_courses["courses"].extend(new_courses)
+                file.seek(0)
+                file.write(json.dumps(past_courses))
+                file.truncate()
+        except FileNotFoundError:
+            pass
 
     def get_new_courses(self) -> list:
         response = requests.get(url=f"{self.base_url}/course?page=1")
@@ -36,7 +37,6 @@ class CourseFlixManager:
             course for course in courses_links if course not in stored_courses
         ]
         if new_courses:
-            self._write_courses(new_courses)
-            self._write_to_counter(len(new_courses))
+            self._save_new_courses(new_courses)
             return new_courses
         return []
